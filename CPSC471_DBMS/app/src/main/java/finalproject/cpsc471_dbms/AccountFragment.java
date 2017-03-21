abfd2faa8f09b58b086e905fe05d40078e85bea9
William Hong <wjhong@shaw.ca>
2017-03-21 14:16:48 -0600

    added actual project files and removed the tar

---
+++ CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/AccountFragment.java
@@ -0,0 +1,27 @@
+package finalproject.cpsc471_dbms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by wj-hong on 26/02/17.
+ */
+
+public class AccountFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.account_tab, container, false);
+
+        //code for this fragment goes here
+
+        return view;
+    }
+}
