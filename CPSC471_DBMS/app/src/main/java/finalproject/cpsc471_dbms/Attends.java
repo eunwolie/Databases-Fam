bfeb5c34fc10a550665027391cd04dbb4aaecb39
eunwolie <kyookiyomi@gmail.com>
2017-03-25 22:52:38 -0600

    Revert "Database Table Creation"
    This reverts commit 2387f6f63d5c57af1d8f7827a4d11c60c559b8ed.

--- CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/Attends.java
+++
@@ -1,8 +0,0 @@
-package finalproject.cpsc471_dbms;
-
-/**
- * Created by evech on 2017-03-25.
- */
-
-public class Attends {
-}
--- CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/Event.java
+++
@@ -1,8 +0,0 @@
-package finalproject.cpsc471_dbms;
-
-/**
- * Created by evech on 2017-03-24.
- */
-
-public class Event {
-}
