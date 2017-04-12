88d57f68accdbe10291adfe6bfb7a9e00f3ddf68
eunwolie <eve.chen@rocketmail.com>
2017-04-12 13:08:00 -0600

    Renamed things from 0-5

--- CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/DatabaseHandler/Librarian.java
+++ CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/DatabaseHandler/Librarian.java
@@ -72,7 +72,7 @@ public class Librarian extends IHandler<LibrarianDef, LibrarianTable>{
     protected List<LibrarianDef> genEntities() {
         List<LibrarianDef> list = new ArrayList<>();
 
-        list.add(new LibrarianDef(7006,4));
+        list.add(new LibrarianDef(0,4));
 
         return list;
     }
