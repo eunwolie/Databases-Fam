a6af3b645109e3aae01c3976c6f00bbf2790ded4
eunwolie <eve.chen@rocketmail.com>
2017-04-12 13:07:10 -0600

    Renamed things from 0-5

--- CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/DatabaseHandler/EventAttendance.java
+++ CPSC471_DBMS/app/src/main/java/finalproject/cpsc471_dbms/DatabaseHandler/EventAttendance.java
@@ -65,12 +65,12 @@ public class EventAttendance extends IHandler<EventAttendanceDef, EventAttendanc
     protected List<EventAttendanceDef> genEntities() {
         List<EventAttendanceDef> list = new ArrayList<>();
 
-        list.add(new EventAttendanceDef(9001,900, 421, 7001));
-        list.add(new EventAttendanceDef(9002,800, 422, 7001));
-        list.add(new EventAttendanceDef(9003,1100, 425, 7001));
-        list.add(new EventAttendanceDef(9004,1200, 425, 7002));
-        list.add(new EventAttendanceDef(9005,1200, 425, 7003));
-        list.add(new EventAttendanceDef(9006,1200, 425, 7004));
+        list.add(new EventAttendanceDef(0, 900, 0, 7001));
+        list.add(new EventAttendanceDef(1, 800, 1, 7001));
+        list.add(new EventAttendanceDef(2, 1100, 2, 7001));
+        list.add(new EventAttendanceDef(3, 1200, 3, 7002));
+        list.add(new EventAttendanceDef(4, 1200, 4, 7003));
+        list.add(new EventAttendanceDef(5, 1200, 5, 7004));
 
         return list;
     }
